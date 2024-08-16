fa50290c1476f84b8c13a9af4d9545fb8de4d7f3
Mohammad <muhamad.zolfaghari@gmail.com>
2024-08-16 21:35:21 +0330

    feat(DateConvertor): add isLeapYear and tests

--- demo/index.ts
+++ demo/index.ts
@@ -1,12 +1,3 @@
-import PersianDate from "persian-date";
+import persianDate from "persian-date";
 
-const date = new PersianDate();
-
-const a = date.toLocaleString("fa-IR-u-nu-latn");
-//   .split("/")
-//   .map((d) =>
-//     d.replace(/[۰-۹]/g, (d) => String(d.charCodeAt(0) - "۰".charCodeAt(0)))
-//   )
-//   .join("-");
-
-console.log(new Date(a), a);
+console.log(persianDate("1403/02/14"));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,58 @@
-export default class PersianDate extends Date {
+import DateConvertor from "./DateConvertor/DateConvertor";
+
+export class PersianDate extends DateConvertor {
+  constructor(date: Date | string | number = new Date()) {
+    super(date);
+
+    this.normalizeDate();
+
+    console.log(this.getFullYear());
+  }
+
+  private normalizeDate() {
+    if (!this.isPersianCalendar()) {
+      this.setFullYear(this.getFullYear() - 621);
+    }
+  }
+
+  // Convert Persian digits to Latin digits
   private toPersianDigits(n: string): string {
     return n.replace(/[۰-۹]/g, (d) =>
       String(d.charCodeAt(0) - "۰".charCodeAt(0))
     );
   }
+
+  // Format date to Persian locale with Latin digits
+  toFaIRLocaleString(options?: Intl.DateTimeFormatOptions): string {
+    if (this.isPersianCalendar()) {
+      return this.toLocaleString(undefined, options);
+    }
+
+    return this.toLocaleString("fa-IR-u-nu-latn", options);
+  }
+
+  // Calculate the difference in time between two PersianDate instances
+  diff(date: PersianDate): number {
+    return Math.abs(this.getTime() - date.getTime());
+  }
 }
+
+// Factory function to create and return a new PersianDate instance
+function persianDate(date?: PersianDate | string | number | Date): PersianDate {
+  // Convert the input to a valid Date or undefined
+  if (date instanceof PersianDate) {
+    return new PersianDate(date.getTime());
+  }
+
+  if (
+    typeof date === "string" ||
+    typeof date === "number" ||
+    date instanceof Date
+  ) {
+    return new PersianDate(date);
+  }
+
+  return new PersianDate(); // If no valid date is provided, return current date
+}
+
+export default persianDate;
