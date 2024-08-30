9cd973191707b50fcedbfa708d55928363cc53cd
muhamad.zolfaghari <muhamad.zolfaghari@gmail.com>
2024-08-30 22:10:54 +0330

    refactor(project): refactor restructure and normalize arguments

--- demo/index.ts
+++ demo/index.ts
@@ -1,5 +1,5 @@
 import { persianDate } from "persian-date";
 
-const a = persianDate("2024-09-02");
+const a = persianDate("2024-09-02", { ignoreCalendar: false });
 
 // console.log(persianDate("2001/02/14").format("YYYY/MM/DD"));
