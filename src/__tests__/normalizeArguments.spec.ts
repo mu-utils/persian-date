27989ad9b8c6a796eef6d6f9e465a0702406958f
muhamad.zolfaghari <muhamad.zolfaghari@gmail.com>
2024-08-30 22:09:30 +0330

    fix(normalizeArguments): resolve method testes

--- src/__tests__/normalizeArguments.spec.ts
+++ src/__tests__/normalizeArguments.spec.ts
@@ -1,35 +1,25 @@
 import normalizeArguments from "../utils/normalizeArguments";
 
 describe("normalizeArguments", () => {
-  it("should return an object with props and options which is undefined", () => {
-    const result = normalizeArguments(["2023/01/01"]);
-    expect(result).toEqual({
-      props: ["2023/01/01"],
-      options: undefined,
-    });
+  it("should return an tuple of props and options", () => {
+    expect(normalizeArguments(["2023/01/01"])).toMatchSnapshot([
+      ["2023/01/01"],
+      undefined,
+    ]);
   });
 
   it("should return an object with props and options which is has an object", () => {
     const result = normalizeArguments(["2023/01/01", { ignoreCalendar: true }]);
-    expect(result).toEqual({
-      props: ["2023/01/01"],
-      options: { ignoreCalendar: true },
-    });
+    expect(result).toEqual([["2023/01/01"], { ignoreCalendar: true }]);
   });
 
   it("should return props with empty array and options which is undefined", () => {
     const result = normalizeArguments([]);
-    expect(result).toEqual({
-      props: [],
-      options: undefined,
-    });
+    expect(result).toEqual([[], undefined]);
   });
 
   it("should return props with empty array and options", () => {
     const result = normalizeArguments([{ ignoreCalendar: true }]);
-    expect(result).toEqual({
-      props: [],
-      options: { ignoreCalendar: true },
-    });
+    expect(result).toEqual([[], { ignoreCalendar: true }]);
   });
 });
--- src/utils/persianDate.ts
+++ src/utils/persianDate.ts
@@ -1,6 +1,5 @@
 import PersianDate from "../PersianDate";
 import PersianDateArguments from "../types/PersianDateArguments";
-import PersianDateConstructor from "../types/PersianDateConstructor";
 import PersianDateOptions from "../types/PersianDateOptions";
 
 function persianDate(...args: PersianDateArguments): PersianDate {
@@ -8,38 +7,3 @@ function persianDate(...args: PersianDateArguments): PersianDate {
 }
 
 export default persianDate;
-
-type MyDateArguments =
-  | []
-  | [number]
-  | [string]
-  | [Date]
-  | [number, number?, number?, number?, number?, number?, number?];
-
-class MyDate extends Date {
-  constructor(options?: PersianDateOptions);
-  constructor(value: Date, options?: PersianDateOptions);
-  constructor(value: number, options?: PersianDateOptions);
-  constructor(value: string, options?: PersianDateOptions);
-  constructor(
-    year: number,
-    month: number,
-    date?: number,
-    hours?: number,
-    minutes?: number,
-    seconds?: number,
-    ms?: number,
-  );
-
-  // write constructor for year month and so on
-
-  constructor() {
-    super();
-  }
-
-  customMethod() {
-    console.log("This is a custom method in MyDate");
-  }
-}
-
-new MyDate();
