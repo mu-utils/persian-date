7689d909bdbe2cda106eb2b72b3dd38777ca2ac4
muhamad.zolfaghari <muhamad.zolfaghari@gmail.com>
2024-08-30 22:10:55 +0330

    chore(demo project): install nodemon

--- demo/index.ts
+++ demo/index.ts
@@ -2,4 +2,6 @@ import { persianDate } from "persian-date";
 
 const a = persianDate("2024-09-02", { ignoreCalendar: false });
 
+console.log(a.format("YYYY/MM/DD"));
+
 // console.log(persianDate("2001/02/14").format("YYYY/MM/DD"));
