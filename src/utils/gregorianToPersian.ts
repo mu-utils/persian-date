6b90e1c59e7152d8dd9888852053d056c810e209
muhamad.zolfaghari <muhamad.zolfaghari@gmail.com>
2024-09-02 14:29:11 +0330

    feat(persian date): add some methods to thath

---
+++ src/utils/gregorianToPersian.ts
@@ -0,0 +1,17 @@
+/**
+ * Converts a Gregorian date to Persian date.
+ *
+ * @param {number} gregorianYear - Gregorian year.
+ * @param {number} gregorianMonth - Gregorian month.
+ * @param {number} gregorianDay - Gregorian day.
+ * @returns {object} Persian date.
+ */
+export function gregorianToPersian(
+  gregorianYear,
+  gregorianMonth,
+  gregorianDay
+) {
+  return julianDayNumberToPersian(
+    gregorianToJulianDayNumber(gregorianYear, gregorianMonth, gregorianDay)
+  );
+}
