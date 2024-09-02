2bc6bfd54f72d18a2e86d9c75233672770fb8e02
muhamad.zolfaghari <muhamad.zolfaghari@gmail.com>
2024-09-02 18:47:44 +0330

    feat(gregorian date): add gregorian date to persian date test and utils

---
+++ src/__tests__/gregorianToPersian.spec.ts
@@ -0,0 +1,7 @@
+import gregorianToPersian from "../utils/gregorianToPersian";
+
+describe("gregorianToPersian", () => {
+  it("convert gregorian date to persian", () => {
+    
+  });
+});
--- src/utils/gregorianToPersian.ts
+++ src/utils/gregorianToPersian.ts
@@ -1,18 +1,21 @@
+import DateTuple from "../types/DateTuple";
 import gregorianToJulianDayNumber from "./gregorianToJulianDayNumber";
+import julianDayNumberToPersian from "./julianDaynumberToPersian";
 
 /**
  * Converts a Gregorian date to Persian date.
+ * In case of Intl is not available, it uses the fallback implementation.
  *
  * @param {number} gregorianYear - Gregorian year.
  * @param {number} gregorianMonth - Gregorian month.
  * @param {number} gregorianDay - Gregorian day.
- * @returns {object} Persian date.
+ * @returns {DateTuple} Persian date in tuple format (year, month, day).
  */
 export function gregorianToPersian(
-  gregorianYear,
-  gregorianMonth,
-  gregorianDay
-) {
+  gregorianYear: number,
+  gregorianMonth: number,
+  gregorianDay: number
+): object {
   return julianDayNumberToPersian(
     gregorianToJulianDayNumber(gregorianYear, gregorianMonth, gregorianDay)
   );
