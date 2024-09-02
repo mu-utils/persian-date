9d9033a563cf46f5bcfa6017d9f9026615f3f63d
muhamad.zolfaghari <muhamad.zolfaghari@gmail.com>
2024-09-02 21:10:43 +0330

    feat(dateUtils): Enhance Persian date handling, add Persian to Julian conversion, and update/remove Gregorian utilities

--- src/PersianDate.ts
+++ src/PersianDate.ts
@@ -2,8 +2,6 @@ import PersianDateOptions from "./types/PersianDateOptions";
 import normalizeArguments from "./utils/normalizeArguments";
 import DateFormatTemplate from "./types/DateFormatTemplate";
 import formatDate from "./utils/formatDate";
-import { toGregorianDate } from "./utils/gregorianDate";
-import validatePersianDate from "./utils/validatePersianDate";
 import DEFAULT_OPTIONS from "./constants/defaultOptions";
 import normalizeTime from "./utils/normalizeTime";
 
@@ -59,7 +57,7 @@ export default class PersianDate extends Date {
     const [newArguments, options] = normalizeArguments(args);
     super(...(newArguments as []));
     this.setOptions(options);
-    this.normalizeTime();
+    this.setTime(normalizeTime(this.getTime(), this.options));
   }
 
   setOptions(options?: PersianDateOptions) {
@@ -68,36 +66,8 @@ export default class PersianDate extends Date {
     }
   }
 
-  private normalizeTime() {
-    const time = this.getTime();
-    this.setTime(normalizeTime(time, this.options));
-
-    // if (result === DateValidationResult.DATE_IS_INVALID) {
-    //   this.setTime(NaN);
-    //   throwInvalidDateError(this.options.invalidDateSeverity);
-    // }
-
-    // if (result === DateValidationResult.PERSIAN_DATE_IS_INVALID) {
-    //   if (!this.options.ignoreCalendar) {
-    //     this.setTime(NaN);
-    //     throwInvalidDateError(this.options.invalidDateSeverity);
-    //   } else {
-    //     this.toPersianDate();
-    //   }
-    // }
-  }
-
   format(template: DateFormatTemplate): string {
     return formatDate(this, template);
-  }
-
-  toGregorianDate() {
-    // return toGregorianDate(this);
-  }
-
-  // Format date to Persian locale with Latin digits
-  toPersianLocaleString(options?: Intl.DateTimeFormatOptions): string {
-    return this.toLocaleString("fa-IR-u-nu-latn", options);
   }
 
   diff(date: PersianDate): number {
