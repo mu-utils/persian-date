74b9fbf24f76a926b231532cd35bfddcdcc4f7f7
muhamad.zolfaghari <muhamad.zolfaghari@gmail.com>
2024-09-03 22:04:19 +0330

    feat(demo): add some examples

--- demo/index.ts
+++ demo/index.ts
@@ -1,14 +1,14 @@
 import { PersianDate, persianDate } from "persian-date";
 
-const persianDate1 = persianDate("2023-06-12T04:05:03", {
+const persianDate1 = persianDate("2023-06-12T16:05:03", {
   calendar: "gregorian",
-  timeZone: "America/New_York"
+  timeZone: "America/New_York",
 });
 
 // console.log(new Date("2020-01-01T13:12:00"));
 
 // console.log(date, new Date());
 
-console.log(new Date());
+// console.log(new Date());
+
 console.log(persianDate1);
-console.log(persianDate1.format("HH:mm:ss"));
